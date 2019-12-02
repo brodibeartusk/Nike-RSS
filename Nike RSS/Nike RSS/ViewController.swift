61036a7b4113b03b55793a75886188ecaaaa7f50
Brodi Beartusk <brodibeartusk@gmail.com>
2019-12-02 12:36:50 -0800

    Create project
    Create Xcode project

---
+++ Nike RSS/Nike RSS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nike RSS
+//
+//  Created by Brodi Beartusk on 12/2/19.
+//  Copyright © 2019 Brodi Beartusk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
